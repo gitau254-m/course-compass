8c6d2223be4c7d716d5f2576f90c59f18ba230da
Wallace Gitau <gitauwallace04@gmail.com>
2026-03-07 17:03:49 +0300

    fix: inline PWA service worker registration

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && componentTagger(),
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: "inline",
       includeAssets: [
         "favicon.ico",
         "favicon.svg",
